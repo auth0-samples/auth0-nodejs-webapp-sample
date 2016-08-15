ed4fbd4e18e8960329e8231ab7fbc51e0a304e1e
Juan Cabrera <jjcp.91@gmail.com>
2016-08-15 11:33:46 -0300

    Add 00-Starter-Seed

---
+++ 00-Starter-Seed/routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var passport = require('passport');
+var router = express.Router();
+
+var env = {
+  AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
+  AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
+  AUTH0_CALLBACK_URL: process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/callback'
+};
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express', env: env });
+});
+
+
+module.exports = router;
