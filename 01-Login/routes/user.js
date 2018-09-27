47b970e0c7a5d4f1b2c1cd99d6c2f95266cb71b3
José F. Romaniello <jfromaniello@gmail.com>
2018-09-27 10:13:09 -0300

    improve the user profile view

--- 01-Login/routes/user.js
+++ 01-Login/routes/user.js
@@ -1,13 +1,15 @@
 const express = require('express');
-const passport = require('passport');
 const ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn();
 const router = express.Router();
 
 /* GET user profile. */
 router.get('/', ensureLoggedIn, function(req, res, next) {
+  const { _raw, _json, ...passportProfile } = req.user;
+
   res.render('user', {
-    user: req.user ,
-    userProfile: JSON.stringify(req.user, null, '  ')
+    user: req.user,
+    passportProfile: JSON.stringify(passportProfile, null, 2),
+    auth0UserInfo: JSON.stringify(_json, null, 2),
   });
 });
 
